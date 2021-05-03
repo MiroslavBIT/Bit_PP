bb8a1f414d7c4c420a6b7e1372a3e7a53b4815c1
BIT Student <classroom@bgit.rs>
2021-05-03 12:02:48 +0200

    initial commit

---
+++ Practice/practice.js
@@ -0,0 +1,36 @@
+var a = 8;
+var b = 7;
+if (a > b) {
+    console.log(a)
+} else { console.log(b) }
+
+
+
+
+var a = 9;
+
+if (a % 2 === 0) {
+    console.log('even')
+} else {
+    console.log("odd")
+}
+
+
+var a = 15;
+if (a % 3 === 0 && a % 5 === 0) {
+    console.log(a)
+}
+
+
+
+var a = 3;
+var b = -7;
+var c = 2;
+if (a * b * c > 0) {
+    console.log("+")
+} else {
+    console.log("-")
+
+}
+
+
---
+++ Practice/variables.js
@@ -0,0 +1,50 @@
+var a = 2;
+var b = 3;
+var _name = "miki";
+
+var c = _name;
+//var 2d = 5;
+var mood = "happy";
+console.log(mood)
+
+var myname = "miroslav";
+console.log(myname)
+
+var a = 3;
+var b = 2;
+var c = 31;
+var result = a - b - c;
+console.log(result);
+
+
+var b = 5;
+var c = 7;
+result = a * b / c;
+console.log(result)
+
+
+var c = 4;
+var d = 1.14;
+result = c / a - d * a;
+console.log(result)
+
+
+var weightBananaInKg = 1.2;
+var KgGrRatio = 1000;
+var weightBananaInGrams = weightBananaInKg * KgGrRatio;
+console.log(weightBananaInGrams)
+
+
+var anaLate = 5;
+var minSecondRatio = 60;
+var anaLateInsecond = anaLate * minSecondRatio;
+console.log(anaLateInsecond)
+var tomLateInMinutes = 30;
+var tomLateinSeconds = tomLateInMinutes * minSecondRatio;
+console.log(tomLateinSeconds)
+
+
+var USBstick = 4;
+
+
+
