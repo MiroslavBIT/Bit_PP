cdddcb11dde3d87985ca0627fb559aa6949fbdc5
BIT student <miroslavtrajkovic@gmail.com>
2021-05-04 13:51:49 +0200

    Practice Arrays-Loops

---
+++ Arrays-practice.js
@@ -0,0 +1,30 @@
+/* 1. Write an array of months in a year. Using console.log display June, October and January
+from the array. */
+
+var mounth = ["January", "February", "March", "Apri", "May", "Jun", "July", "August", "September", "Oktober", "November", "December"]
+
+console.log(mounth[5], mounth[9], mounth[1]);
+
+
+/*2. Write an arrays of days in a week. Using console.log display Sunday from the array. */
+
+var day = ["Monday", "Tusday", "Wendsday", "Thursday", "Friday", "Suterday", "Sunday"]
+
+console.log(day[6]);
+
+
+/*3. Print all negative elements from the array [2, -4, 5, -2, -11]. */
+
+
+var number = [2, -4, 5, -2, -11]
+
+console.log(number[1], number[3], number[4]);
+
+/*4. Print all elements with indices divisible by 3 from the array [5, 15, -5, 20, 12, 18,
+72, 14, 9]. */
+
+var number = [5, 15, -5, 20, 12, 18, 72, 14, 9]
+
+console.log(number[1], number[4], number[5], number[6], number[8]);
+
+
